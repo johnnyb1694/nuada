ad037d9b4859a801837fe72f0b729f5f1462919b
Johnny Breen <johnnyb1694@gmail.com>
2024-01-27 15:34:40 +0000

    Added the Guardian as a possible source

--- src/nuada/api/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-The `nuada` REST API permits users to access term-frequency resources from various media outlets over the web.
-'''
-from fastapi import FastAPI
-
-app = FastAPI()
-
-
-
--- src/nuada/pipeline/resources.py
+++ src/nuada/pipeline/resources.py
@@ -1,62 +1,109 @@
 import requests
 import pandas as pd
+import time
+import logging
+import itertools
 
-class Resource():
+from datetime import datetime, timedelta
+from requests.exceptions import RequestException
 
-    def __init__(self, url: str, params: dict):
-        self.url = url
-        self.params = params
-        self.response = None
+def request(url: str, params: dict, delay: int = 0):
+    '''
+    Wrapper function which calls `requests.get()` under the hood
+    '''
+    if delay > 0:
+        logging.info(f'Sleeping for {delay} seconds prior to making GET request (endpoint: {url})')
+        time.sleep(delay)
+    res = requests.get(url, params)
+    res.raise_for_status()
+    deserialised = res.json()
+    return deserialised
 
-    def load(self):
-        try:
-            response = requests.get(self.url, self.params)
-            response.raise_for_status()
-        except requests.exceptions.RequestException as e:
-            raise e
-        else:
-            self.response = response.json()
+def get_date_range(year: int, month: int):
+    '''
+    For a given year and month, this function returns the 'start' and 'end' dates that enclose that specific month
+    '''
+    start = datetime(year, month, 1)
+    if month == 12:
+        end = datetime(year + 1, 1, 1)
+    else:
+        end = datetime(year, month + 1, 1)
+    return start.date(), end.date()
 
-    def __repr__(self):
-        return f'Resource @ "{self.url}"'
+def standardise_guardian_headlines(res_deserialised: dict):
+    '''
+    Ingest raw JSON response from the Guardian API resource (`deserialised`) and standardise into a list of
+    dictionaries with fields: `publication_date` and `headline`
+    '''
+    articles = res_deserialised['response']['results']
+    headlines = [{'publication_date': pd.to_datetime(article['webPublicationDate']),
+                  'headline': article['webTitle']} for article in articles]
+    return headlines
 
-class NewYorkTimes(Resource):
+def standardise_nyt_headlines(res_deserialised: dict):
+    '''
+    Ingest raw JSON response from the New York Times API resource (`deserialised`) and standardise into a list of
+    dictionaries with fields: `publication_date` and `headline`
+    '''
+    articles = res_deserialised['response']['docs']
+    headlines = [{'publication_date': pd.to_datetime(article['pub_date']),
+                  'headline': article['headline']['main']} for article in articles]
+    return headlines
 
-    def __init__(self, year: int, month: int, nyt_key: str):
-        nyt_url = f'https://api.nytimes.com/svc/archive/v1/{str(year)}/{str(month)}.json'
-        super().__init__(url=nyt_url, params={'api-key': nyt_key})
-
-    def preprocess(self) -> pd.DataFrame:
-
-        articles = self.response_raw['response']['docs']
-
-        headlines = [{'publication_date': pd.to_datetime(article['pub_date']),
-                      'headline': article['headline']['main']} for article in articles]
-        
-        headlines_df = pd.DataFrame(headlines)
-        headlines_df['year'] = headlines_df['publication_date'].dt.year
-        headlines_df['month'] = headlines_df['publication_date'].dt.month
-        return headlines_df
-
-def request_nyt_archive_search(year: int, month: int, key: str) -> str:
-    """
-    Request results of the New York Times 'Archive Search' API (NYT API) via the internet.
+def request_nyt_headlines(year: int, month: int, key: str) -> str:
+    '''
+    Get all of the headlines from the New York Times for a specific `year` & `month`
 
     :param year: Year of interest
     :param month: Month of interest
-    :param key: API key applicable to the NYT API
-    """
+    :param key: Developer key for New York Times API service
+    '''
     if not key:
         raise ValueError('Input variable `key` must be specified')
-    
-    url = f'https://api.nytimes.com/svc/archive/v1/{str(year)}/{str(month)}.json?api-key={key}'
+    url = f'https://api.nytimes.com/svc/archive/v1/{str(year)}/{str(month)}.json'
+    params = {'api-key': key}
     try:
-        res = requests.get(url)
-        res.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        raise e
-    return res.json()
+        res = request(url, params)
+        headlines = standardise_nyt_headlines(res)
+    except RequestException as err:
+        raise err
+    return headlines
+
+def impute_param(params: dict, key: str, value):
+    params[key] = value
+    return params
+
+def request_guardian_headlines(year: int, month: int, key: str, n_pages: int | None = None) -> str:
+    '''
+    Get all of the headlines from the Guardian for a specific `year` & `month`
+
+    :param year: Year of interest
+    :param month: Month of interest
+    :param key: Developer key for Guardian API service
+    '''
+    if not key:
+        raise ValueError('Input variable `key` must be specified')
+    start_date, end_date = get_date_range(year, month)
+    url = 'https://content.guardianapis.com/search'
+    params = {'api-key': key,
+              'page': 1,
+              'page-size': 50,
+              'from-date': str(start_date),
+              'to-date': str(end_date)}
+    try:
+        if not n_pages:
+            init_res = request(url, params)
+            n_pages = init_res['response']['pages'] # NB: retrieves the number of pages
+        headline_list = []
+        for n in range(1, n_pages + 1):
+            params['page'] = n
+            res = request(url, params, delay = 0.50)
+            headlines = standardise_guardian_headlines(res)
+            headline_list.append(headlines)
+        headlines = list(itertools.chain.from_iterable(headline_list))
+    except RequestException as err:
+        raise err
+    return headlines
 
 if __name__ == '__main__':
-    nyt = NewYorkTimes(2023, 11, 'ePMmMmWGTVXZozP9iIzwTVqEYLyoRCuv')
-    nyt_headlines = nyt.load().preprocess()
+    pass
