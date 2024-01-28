71a7bb571b0d33a77e85824d23a76b19a8de9da8
johnnyb1694 <johnnyb1694@gmail.com>
2024-01-28 18:36:59 +0000

    Shoring up the resources and transformer module

--- src/nuada/pipeline/__init__.py
+++ src/nuada/pipeline/__init__.py
@@ -2,7 +2,7 @@
 A project dedicated to extracting data from freely available news outlet APIs (e.g. the New York Times and the Guardian) to understand topic frequencies & trends.
 """
 
-from nuada.pipeline.transformer import preprocess
+from nuada.pipeline.transformer import transform
 from nuada.pipeline.db import DBC, init_db, ingest
 from nuada.models import Control
-from nuada.pipeline.resources import request_nyt_archive_search
+from nuada.pipeline.resources import request_guardian_headlines, request_nyt_headlines
--- src/nuada/pipeline/resources.py
+++ src/nuada/pipeline/resources.py
@@ -7,19 +7,28 @@ import itertools
 from datetime import datetime, timedelta
 from requests.exceptions import RequestException
 
-def request(url: str, params: dict, delay: int = 0):
+def _convert_headlines_to_df(headlines: list[dict]) -> pd.DataFrame:
+    '''
+    Convert the list of dictionary objects returned by standardisation functionality into a `pd.DataFrame` object
+    '''
+    headlines_df = pd.DataFrame(headlines)
+    headlines_df['year'] = headlines_df['publication_date'].dt.year
+    headlines_df['month'] = headlines_df['publication_date'].dt.month
+    return headlines_df
+
+def _request(url: str, params: dict, delay: int = 0):
     '''
     Wrapper function which calls `requests.get()` under the hood
     '''
     if delay > 0:
-        logging.info(f'Sleeping for {delay} seconds prior to making GET request (endpoint: {url})')
+        logging.info(f'Sleeping for {delay} seconds prior to making GET request (target endpoint: "{url}")')
         time.sleep(delay)
     res = requests.get(url, params)
     res.raise_for_status()
     deserialised = res.json()
     return deserialised
 
-def get_date_range(year: int, month: int):
+def _get_date_range(year: int, month: int):
     '''
     For a given year and month, this function returns the 'start' and 'end' dates that enclose that specific month
     '''
@@ -28,9 +37,10 @@ def get_date_range(year: int, month: int):
         end = datetime(year + 1, 1, 1)
     else:
         end = datetime(year, month + 1, 1)
+    end = end - timedelta(days=1)
     return start.date(), end.date()
 
-def standardise_guardian_headlines(res_deserialised: dict):
+def _standardise_guardian_headlines(res_deserialised: dict):
     '''
     Ingest raw JSON response from the Guardian API resource (`deserialised`) and standardise into a list of
     dictionaries with fields: `publication_date` and `headline`
@@ -40,7 +50,7 @@ def standardise_guardian_headlines(res_deserialised: dict):
                   'headline': article['webTitle']} for article in articles]
     return headlines
 
-def standardise_nyt_headlines(res_deserialised: dict):
+def _standardise_nyt_headlines(res_deserialised: dict):
     '''
     Ingest raw JSON response from the New York Times API resource (`deserialised`) and standardise into a list of
     dictionaries with fields: `publication_date` and `headline`
@@ -63,15 +73,12 @@ def request_nyt_headlines(year: int, month: int, key: str) -> str:
     url = f'https://api.nytimes.com/svc/archive/v1/{str(year)}/{str(month)}.json'
     params = {'api-key': key}
     try:
-        res = request(url, params)
-        headlines = standardise_nyt_headlines(res)
+        res = _request(url, params)
+        headlines = _standardise_nyt_headlines(res)
+        headlines_df = _convert_headlines_to_df(headlines)
     except RequestException as err:
         raise err
-    return headlines
-
-def impute_param(params: dict, key: str, value):
-    params[key] = value
-    return params
+    return headlines_df
 
 def request_guardian_headlines(year: int, month: int, key: str, n_pages: int | None = None) -> str:
     '''
@@ -80,10 +87,11 @@ def request_guardian_headlines(year: int, month: int, key: str, n_pages: int | N
     :param year: Year of interest
     :param month: Month of interest
     :param key: Developer key for Guardian API service
+    :param n_pages: Number of pages to search for; defaults to `None` in which case the number is detected from the API service
     '''
     if not key:
         raise ValueError('Input variable `key` must be specified')
-    start_date, end_date = get_date_range(year, month)
+    start_date, end_date = _get_date_range(year, month)
     url = 'https://content.guardianapis.com/search'
     params = {'api-key': key,
               'page': 1,
@@ -92,18 +100,20 @@ def request_guardian_headlines(year: int, month: int, key: str, n_pages: int | N
               'to-date': str(end_date)}
     try:
         if not n_pages:
-            init_res = request(url, params)
-            n_pages = init_res['response']['pages'] # NB: retrieves the number of pages
+            init_res = _request(url, params)
+            n_pages = init_res['response']['pages'] 
         headline_list = []
         for n in range(1, n_pages + 1):
             params['page'] = n
-            res = request(url, params, delay = 0.50)
-            headlines = standardise_guardian_headlines(res)
+            res = _request(url, params, delay = 0.50)
+            headlines = _standardise_guardian_headlines(res)
             headline_list.append(headlines)
         headlines = list(itertools.chain.from_iterable(headline_list))
+        headlines_df = _convert_headlines_to_df(headlines)
     except RequestException as err:
         raise err
-    return headlines
+    return headlines_df
 
 if __name__ == '__main__':
+    headlines_guardian = request_guardian_headlines(year=2023, month=12, key='79966749-c95b-458e-84e0-e82a6fe2cf9f', n_pages = 10)
     pass
